a95f269b629e542d0ca2bf5f9de3112b1816b0b4
Quintessence Anx <quintessenceanx@gmail.com>
2014-12-19 13:15:24 -0500

    Solved Problem 2. Updated print for problem 1

--- problem1/problem1.go
+++ problem1/problem1.go
@@ -21,11 +21,12 @@ func sumOfArrayElements(integerArray []int) int {
 }
 
 func main() {
-	// Prompt:
-	//If we list all the natural numbers below 10 that are multiples of 3 or 5, we
-	//get 3, 5, 6 and 9. The sum of these multiples is 23.
-	//Find the sum of all the multiples of 3 or 5 below 1000.
-
-	fmt.Printf("The solution to Euler Problem 1 is: %d\n", sumOfArrayElements(setListOfMultiples(1000, 3, 5)))
+	/* Prompt:
+	If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+	get 3, 5, 6 and 9. The sum of these multiples is 23.
+	Find the sum of all the multiples of 3 or 5 below 1000.
+	*/
+	fmt.Println("The solution to Euler Problem 2 is: ")
+	fmt.Println(sumOfArrayElements(setListOfMultiples(1000, 3, 5)))
 
 }
