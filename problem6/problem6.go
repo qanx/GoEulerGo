775f7b80d989c6b8e545631077710c12241c7ed7
Quintessence Anx <quintessenceanx@gmail.com>
2014-12-28 18:32:10 -0500

    Solved Problem 6

---
+++ problem6/problem6.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func setSequentialSlice(maxValue int) []int {
+	sequentialSlice := []int{1}
+	for i := 1; i < maxValue; i++ {
+		sequentialSlice = append(sequentialSlice, i+1)
+	}
+	return sequentialSlice
+}
+
+func setSumOfSquareOfElements(inputSlice []int) int {
+	sum := 0
+	for i := 0; i < len(inputSlice); i++ {
+		sum += int(math.Pow(float64(inputSlice[i]), 2))
+	}
+	return sum
+}
+
+func setSumOfElements(inputSlice []int) int {
+	sum := 0
+	for i := 0; i < len(inputSlice); i++ {
+		sum += inputSlice[i]
+	}
+	return sum
+}
+
+func main() {
+	/* The sum of the squares of the first ten natural numbers is:
+	(1^2) + (2^2) + ... + (10^2) = 385
+	The square of the sum of the first ten natural numbers is:
+	(1 + 2 + ... + 10)^2 = 3025
+	Hence the difference between the sum of the squares of the first ten natural
+	numbers and the square of the sum is 3025 − 385 = 2640.
+
+	Find the difference between the sum of the squares of the first one hundred
+	natural numbers and the square of the sum.
+	*/
+	fmt.Println("The solution to Euler Problem 6 is: ")
+	sequentialSlice := setSequentialSlice(100)
+	sumOfSquaredElements := setSumOfSquareOfElements(sequentialSlice)
+	sumOfElementsSquared := int(math.Pow(float64(setSumOfElements(sequentialSlice)), 2))
+	fmt.Println(sumOfElementsSquared - sumOfSquaredElements)
+}
